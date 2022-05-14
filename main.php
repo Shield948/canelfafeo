3f19e24380332e24a6edb28e0fffa763950bdaa3
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-14 14:33:10 -0500

    Update main.php

--- main.php
+++ main.php
@@ -34,5 +34,77 @@ $messagesec",
 
 //////////////===[CMDS]===//////////////
 
+if(strpos($message, "/cmds") === 0 || strpos($message, "!cmds") === 0){
+
+  if(!isBanned($userId) && !isMuted($userId)){
+    bot('sendmessage',[
+    'chat_id'=>$chat_id,
+    'text'=>"<b>Which commands would you like to check?</b>",
+    'parse_mode'=>'html',
+    'reply_to_message_id'=> $message_id,
+    'reply_markup'=>json_encode(['inline_keyboard'=>[
+    [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
+    ],'resize_keyboard'=>true])
+    ]);
+  }
+  
+  }
+  
+  if($data == "back"){
+    bot('editMessageText',[
+    'chat_id'=>$callbackchatid,
+    'message_id'=>$callbackmessageid,
+    'text'=>"<b>Which commands would you like to check?</b>",
+    'parse_mode'=>'html',
+    'reply_markup'=>json_encode(['inline_keyboard'=>[
+    [['text'=>"💳 CC Checker Gates",'callback_data'=>"checkergates"]],[['text'=>"🛠 Other Commands",'callback_data'=>"othercmds"]],
+    ],'resize_keyboard'=>true])
+    ]);
+  }
+  
+  if($data == "checkergates"){
+    bot('editMessageText',[
+    'chat_id'=>$callbackchatid,
+    'message_id'=>$callbackmessageid,
+    'text'=>"<b>━━CC Checker Gates━━</b>
+  
+<b>/ss | !ss - Stripe [Auth]</b>
+<b>/sm | !sm - Stripe [Merchant]</b>
+<b>/schk | !schk - User Stripe Merchant [Needs SK]</b>
+
+<b>/apikey sk_live_xxx - Add SK Key for /schk gate</b>
+<b>/myapikey | !myapikey - View the added SK Key for /schk gate</b>
+
+<b>ϟ Join <a href='t.me/IndianBots'>IndianBots</a></b>",
+    'parse_mode'=>'html',
+    'disable_web_page_preview'=>true,
+    'reply_markup'=>json_encode(['inline_keyboard'=>[
+  [['text'=>"Return",'callback_data'=>"back"]]
+  ],'resize_keyboard'=>true])
+  ]);
+  }
+  
+  
+  if($data == "othercmds"){
+    bot('editMessageText',[
+    'chat_id'=>$callbackchatid,
+    'message_id'=>$callbackmessageid,
+    'text'=>"<b>━━Other Commands━━</b>
+  
+<b>/me | !me</b> - Your Info
+<b>/stats | !stats</b> - Checker Stats
+<b>/key | !key</b> - SK Key Checker
+<b>/bin | !bin</b> - Bin Lookup
+<b>/iban | !iban</b> - IBAN Checker
+  
+  <b>ϟ Join <a href='t.me/IndianBots'>IndianBots</a></b>",
+    'parse_mode'=>'html',
+    'disable_web_page_preview'=>true,
+    'reply_markup'=>json_encode(['inline_keyboard'=>[
+  [['text'=>"Return",'callback_data'=>"back"]]
+  ],'resize_keyboard'=>true])
+  ]);
+  }
+
 
 ?>
